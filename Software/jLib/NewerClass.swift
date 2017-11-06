0fef6f44b84de3a2becc1c732002c433bcfc2f8f
justinmreina <justinmreina@gmail.com>
2017-11-06 11:10:08 -0800

    (+)    Generate jLib Framework

---
+++ Software/jLib/NewerClass.swift
@@ -0,0 +1,23 @@
+//
+//  NewerClass Framework
+//
+//  Created by Justin Reina on 11/6/17.
+//  Copyright © 2017 Jaostech. All rights reserved.
+//
+
+import Foundation
+
+
+class NewerClass : NSObject {
+    
+    override init() {
+        print("A Newer Class initiated.");
+        return;
+    }
+    
+    func fcn2() {
+        print("Function Two was called.");
+        return;
+    }
+    
+}
---
+++ Software/jLib/jLib/jLib.h
@@ -0,0 +1,19 @@
+//
+//  jLib.h
+//  jLib
+//
+//  Created by Justin Reina on 11/6/17.
+//  Copyright © 2017 Jmr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for jLib.
+FOUNDATION_EXPORT double jLibVersionNumber;
+
+//! Project version string for jLib.
+FOUNDATION_EXPORT const unsigned char jLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <jLib/PublicHeader.h>
+
+
