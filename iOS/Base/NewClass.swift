0ad018a9e4b0fc3e1ccadf409f7e9df98b1e9414
justinmreina <justinmreina@gmail.com>
2017-12-23 10:21:43 -0800

    (+)    add NewClass.swift

---
+++ iOS/Base/NewClass.swift
@@ -0,0 +1,79 @@
+/************************************************************************************************************************************/
+/** @file       NewClass.swift
+ *  @project    x
+ *  @brief      x
+ *  @details    x
+ *
+ *  @author     Justin Reina, Firmware Engineer, Jaostech
+ *  @created    x
+ *  @last rev   x
+ *
+ *
+ *  @notes      x
+ *
+ *  @section    Opens
+ *      none current
+ *
+ *  @section    Legal Disclaimer
+ *      All contents of this source file and/or any other Jaostech related source files are the explicit property on Jaostech
+ *      Corporation. Do not distribute. Do not copy.
+ */
+/************************************************************************************************************************************/
+import UIKit
+
+//@note     global vars
+
+
+class NewClass : NSObject {
+
+    //@note     class vars
+    
+    /********************************************************************************************************************************/
+    /** @fcn        init()
+     *  @brief      x
+     *  @details    x
+     *
+     *  @section    Purpose
+     *      x
+     *
+     *  @param      [in]  name    descrip
+     *
+     *  @param      [out] name    descrip
+     *
+     *  @return     (type) descrip
+     *
+     *  @pre        x
+     *
+     *  @post       x
+     *
+     *  @section    Operation
+     *      x
+     *
+     *  @section    Opens
+     *      x
+     *
+     *  @section    Hazards & Risks
+     *      x
+     *
+     * @section    Todo
+     *     x
+     *
+     * @section    Timing
+     *     x
+     *
+     *  @note       x
+     */
+    /********************************************************************************************************************************/
+    override init() {
+        
+        //@note     initialization of additional vars
+        
+        super.init();
+        
+        //@note     finalization of initialization
+        
+        print("NewClass.init():    initialization complete");
+
+        return;
+    }
+}
