5c38cc02e16e13693f6322ba61485f623770a244
justinmreina <justinmreina@gmail.com>
2018-01-03 11:20:35 -0800

    (B)    StdComments typo correct

--- iOS/Base/Stdcomments.swift
+++ iOS/Base/Stdcomments.swift
@@ -15,7 +15,7 @@
  * 			none current
  *
  * 	@section	Legal Disclaimer
- * 			All contents of this source file and/or any other Jaostech related source files are the explicit property on Jaostech
+ * 			All contents of this source file and/or any other Jaostech related source files are the explicit property of Jaostech
  * 			Corporation. Do not distribute. Do not copy.
  */
 /************************************************************************************************************************************/
